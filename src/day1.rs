d708a5785691ac449e98d079376cb0afd408b169
Jovasa <jovasa46@gmail.com>
2021-12-02 17:34:06 +0200

    Improve day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,4 @@
 use crate::util::read_file_of_numbers;
-use itertools::izip;
 
 mod util;
 
@@ -12,18 +11,18 @@ fn main() {
 
 fn first(numbers: &Vec<i32>) {
     let mut increasing = 0;
-    let mut previous = numbers.first().unwrap();
-
-    for number in numbers.iter().skip(1) {
-        if number > previous {
-            increasing += 1;
+    for a in numbers.windows(2) {
+        if let [first, second] = a {
+            increasing += (first < second) as i32;
         }
-        previous = number;
     }
     println!("{}", increasing);
 }
 
 fn second(numbers: &Vec<i32>) {
-    let n: Vec<i32> = izip!(numbers.iter(), numbers.iter().skip(1), numbers.iter().skip(2)).map(|(x, y, z)| x +y + z).collect();
-    first(n.as_ref());
+    let i = numbers
+        .windows(3)
+        .map(|pair| (pair[0] + pair[1] + pair[2]))
+        .collect::<Vec<i32>>();
+    first(i.as_ref());
 }
