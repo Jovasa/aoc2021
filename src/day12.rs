ba0dfaa4f31c2c4f0a5fcdf5c3dd63f260e53e4c
Jovasa <jovasa46@gmail.com>
2021-12-13 20:02:00 +0200

    Further optimize day 12

--- src/day12.rs
+++ src/day12.rs
@@ -2,19 +2,9 @@ use std::collections::{HashMap};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn has_visited_small_cave_once(route: &Vec<u64>) -> bool {
-    let mut visited = 0u64;
-    for i in route {
-        if (i & 1 << 63) == 0  {
-            if visited & i != 0 {
-                return true;
-            } else {
-                visited |= i;
-            }
-        }
-    }
-    false
-}
+const UPPERCASE_MASK: u64 = 0x7fff << 15;
+const VISITED_SMALL: u64 = 1u64 << 63;
+const LAST_MASK: u64 = 0x3fffffffu64 << 30; // 30 bits
 
 fn main() {
     let reader = BufReader::new(File::open("data/day12.txt").unwrap());
@@ -23,6 +13,7 @@ fn main() {
     let mut used_edges = HashMap::new();
     used_edges.insert("start".to_owned(), 0u64);
     used_edges.insert("end".to_owned(), 1);
+
     for lin in reader.lines() {
         let lin = lin.unwrap();
         let split = lin.split("-");
@@ -31,28 +22,35 @@ fn main() {
             let i1 = used_edges.len();
             let t = i.to_owned();
             if !used_edges.contains_key(&t) {
-                let is_uppercase = t.chars().next().unwrap().is_uppercase();
-                used_edges.insert(t.to_owned(), (1 << i1) + ((is_uppercase as u64 ) << 63 ));
+                let is_uppercase = if t.chars().next().unwrap().is_uppercase() {15} else { 0 };
+                used_edges.insert(t.to_owned(), 1 << (i1 + is_uppercase));
             }
             temp_stuff.push(used_edges[&t]);
         }
+
         edges.entry(temp_stuff[0]).or_insert(Vec::new()).push(temp_stuff[1]);
         edges.entry(temp_stuff[1]).or_insert(Vec::new()).push(temp_stuff[0]);
     }
 
-    let mut work_set = vec![vec![0]];
+    let mut work_set = vec![0];
+    let mut total = 0;
     let mut in_end = Vec::new();
 
     while work_set.len() != 0 {
         let mut temp = Vec::new();
         for item in work_set {
-            let last = item.last().unwrap();
-            for i in &edges[last] {
+            let last = (item & LAST_MASK) >> 30;
+            for i in &edges[&last] {
                 if *i == 0 { continue; };
-                if (i & 1 << 63) != 0 || !item.contains(i) || !has_visited_small_cave_once(&item) {
-                    let mut a = item.clone();
-                    a.push(i.to_owned());
+                let not_visited_before = item & i == 0;
+                let large_cavern = (i & UPPERCASE_MASK) != 0;
+                if large_cavern || not_visited_before || item & VISITED_SMALL == 0 {
+                    let mut a = item | i;
+                    a &= !LAST_MASK;
+                    a |= i << 30;
+                    if !large_cavern && !not_visited_before { a |= VISITED_SMALL }
                     if *i == 1 {
+                        total += 1;
                         in_end.push(a);
                     } else {
                         temp.push(a);
@@ -62,5 +60,5 @@ fn main() {
         }
         work_set = temp;
     }
-    println!("{}", in_end.len())
+    println!("{}", total)
 }
