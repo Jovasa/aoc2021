960386e1f876522a39e40141cbf8001ad5434a55
Jovasa <jovasa46@gmail.com>
2021-12-13 17:05:43 +0200

    Optimize day 12

--- src/day12.rs
+++ src/day12.rs
@@ -1,15 +1,15 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn has_visited_small_cave_once(route: &Vec<String>) -> bool {
-    let mut visited = HashSet::new();
+fn has_visited_small_cave_once(route: &Vec<u64>) -> bool {
+    let mut visited = 0u64;
     for i in route {
-        if i == &i.to_lowercase() {
-            if visited.contains(i) {
+        if (i & 1 << 63) == 0  {
+            if visited & i != 0 {
                 return true;
             } else {
-                visited.insert(i);
+                visited |= i;
             }
         }
     }
@@ -20,16 +20,27 @@ fn main() {
     let reader = BufReader::new(File::open("data/day12.txt").unwrap());
 
     let mut edges = HashMap::new();
+    let mut used_edges = HashMap::new();
+    used_edges.insert("start".to_owned(), 0u64);
+    used_edges.insert("end".to_owned(), 1);
     for lin in reader.lines() {
         let lin = lin.unwrap();
-        let mut split = lin.split("-");
-        let a = split.next().unwrap().to_owned();
-        let b = split.next().unwrap().to_owned();
-        edges.entry(a.to_owned()).or_insert(Vec::new()).push(b.to_owned());
-        edges.entry(b.to_owned()).or_insert(Vec::new()).push(a.to_owned());
+        let split = lin.split("-");
+        let mut temp_stuff = Vec::new();
+        for i in split {
+            let i1 = used_edges.len();
+            let t = i.to_owned();
+            if !used_edges.contains_key(&t) {
+                let is_uppercase = t.chars().next().unwrap().is_uppercase();
+                used_edges.insert(t.to_owned(), (1 << i1) + ((is_uppercase as u64 ) << 63 ));
+            }
+            temp_stuff.push(used_edges[&t]);
+        }
+        edges.entry(temp_stuff[0]).or_insert(Vec::new()).push(temp_stuff[1]);
+        edges.entry(temp_stuff[1]).or_insert(Vec::new()).push(temp_stuff[0]);
     }
 
-    let mut work_set = vec![vec!["start".to_owned()]];
+    let mut work_set = vec![vec![0]];
     let mut in_end = Vec::new();
 
     while work_set.len() != 0 {
@@ -37,11 +48,11 @@ fn main() {
         for item in work_set {
             let last = item.last().unwrap();
             for i in &edges[last] {
-                if i == "start" { continue; };
-                if i != &i.to_lowercase() || !item.contains(i) || !has_visited_small_cave_once(&item) {
+                if *i == 0 { continue; };
+                if (i & 1 << 63) != 0 || !item.contains(i) || !has_visited_small_cave_once(&item) {
                     let mut a = item.clone();
                     a.push(i.to_owned());
-                    if i == "end" {
+                    if *i == 1 {
                         in_end.push(a);
                     } else {
                         temp.push(a);
